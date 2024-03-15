d2a45afe6fcd43135a3ac9dde8df49d50d98bf23
amar528 <162648880+amar528@users.noreply.github.com>
2024-03-15 12:38:33 +1100

    Merge pull request #12
    * More hackerrank
    
    * More hackerrank
    
    * More hackerrank
    
    * Merge remote-tracking branch 'origin/main' into develop

--- hacker-rank/8.py
+++ hacker-rank/8.py
@@ -6,3 +6,9 @@ if __name__ == '__main__':
         scores = list(map(float, line))
         student_marks[name] = scores
     query_name = input()
+
+    marks = student_marks.get(query_name)
+    if not marks:
+        raise IndexError('That student does not exist.')
+
+    print('%.2f' % (sum(marks) / len(marks)))
