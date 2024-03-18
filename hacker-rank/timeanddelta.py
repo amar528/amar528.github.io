d79e2b4280d02a73be96b9c1dee6f5a9a956840f
amar528 <amar.om@icloud.com>
2024-03-18 12:08:49 +1100

    Finish timeanddelta

--- hacker-rank/timeanddelta.py
+++ hacker-rank/timeanddelta.py
@@ -1,10 +1,9 @@
 #!/bin/python3
-import datetime
-import os
 import time
+import datetime
 
 # Sun 10 May 2015 13:54:36 - 0700
-FORMAT = '%a %d %b %Y %H:%M:%S - %z'
+FORMAT = '%a %d %b %Y %H:%M:%S %z'
 
 
 # Complete the time_delta function below.
@@ -12,9 +11,13 @@ def time_delta(t1, t2):
     t1_strip = time.strptime(t1, FORMAT)
     t2_strip = time.strptime(t2, FORMAT)
 
-    t1_offset = t1_strip.tm_gmtoff
+    t1_off = t1_strip.tm_gmtoff
+    t2_off = t2_strip.tm_gmtoff
 
-    return 0
+    t1_secs = time.mktime(t1_strip) - t1_off
+    t2_secs = time.mktime(t2_strip) - t2_off
+
+    return str(int(abs(t1_secs - t2_secs)))
 
 
 if __name__ == '__main__':
@@ -28,4 +31,4 @@ if __name__ == '__main__':
 
         delta = time_delta(t1, t2)
 
-        print(delta + '\n')
+        print(delta)
