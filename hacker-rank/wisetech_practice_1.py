b157ee75fb9eaa5a39b86ced1a243c4765b9b699
amar528 <amar.om@icloud.com>
2024-03-20 13:02:09 +1100

    Wisetech degree inital, working

--- hacker-rank/wisetech_practice_1.py
+++ hacker-rank/wisetech_practice_1.py
@@ -16,19 +16,34 @@ import sys
 
 def degreeOfArray(arr):
     # the degree is the largest number of times an element occurs in the array
+    # tuple is (number, count, index)
     counts_arr = [(i, arr.count(i), idx) for idx, i in enumerate(arr)]
     max_element = max(counts_arr, key=lambda t: t[1])
     degree = max_element[1]
 
-    max_elements = list(filter(lambda e: e[1] == degree, counts_arr))
-    max_elements.sort(key=lambda e: e[0])
+    elements_with_degree = list(filter(lambda e: e[1] == degree, counts_arr))
+    elements_with_degree.sort(key=lambda e: e[0])
 
     # given the degree of arr compute all sub arrays that have the same degree
-    sub_arrays = [sub_array for sub_array in arr]
+
+    # key numbers by their indices
+    number_indices = {}
+    for e in elements_with_degree:
+        k = e[0]
+        index_to_add = e[2]
+        v = number_indices.get(k)
+        if v is not None:
+            v.append(index_to_add)
+        else:
+            number_indices[k] = [index_to_add]
+
+    # construct sub arrays for each key (number) using the min, max indices
+    lengths = []
+    for v in number_indices.values():
+        lengths.append(min(v) + max(v) + 1)
 
     # return the minimum length of the sub arrays
-
-    return degree
+    return min(lengths)
 
 
 if __name__ == '__main__':
