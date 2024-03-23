325960487ea56a29dba51d9dde8b9276cb07c45b
amar528 <amar.om@icloud.com>
2024-03-24 09:08:55 +1100

    Step 1

---
+++ rplife/rplife/grid.py
@@ -0,0 +1,12 @@
+class LifeGrid:
+    def __init__(self, pattern):
+        self.pattern = pattern
+
+    def evolve(self):
+        pass
+
+    def as_string(self, bbox):
+        pass
+
+    def __str__(self):
+        pass
