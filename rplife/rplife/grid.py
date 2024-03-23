df480d79fc70e2ba354cab3c79aac1bc9cb57614
amar528 <amar.om@icloud.com>
2024-03-24 09:14:22 +1100

    add evolve definition

--- rplife/rplife/grid.py
+++ rplife/rplife/grid.py
@@ -2,7 +2,20 @@ class LifeGrid:
     def __init__(self, pattern):
         self.pattern = pattern
 
+    # determine the cells that will pass to the next generation as living cells
     def evolve(self):
+        # check currently alive cells and their neighbours
+        # to determine the number of neighbours
+        # decide which cells stay alive, which die, which comes alive
+
+        # 1. alive cells die IF they have:
+        #   < 2 living neighbours (underpopulation
+        #   > 3 living neighbours (overpopulation)
+        # 2. alive cells stay alive IF they have:
+        #   2 OR 3 living neighbours
+        # 3. dead cells become alive IF they have:
+        #   exactly 3 living neighbours (reproduction)
+
         pass
 
     def as_string(self, bbox):
