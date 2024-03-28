dba2a7637bb4b01bcc50224e7cf93c5eca448f75
amar528 <amar.om@icloud.com>
2024-03-28 17:26:33 +1100

    # Conflicts: #	hacker-rank/connected_cells_grid.py #	hacker-rank/test/test_connected_cells_grid.py

--- hacker-rank/test/test_connected_cells_grid.py
+++ hacker-rank/test/test_connected_cells_grid.py
@@ -26,3 +26,27 @@ class Test(TestCase):
 
         result = get_largest_region(grid)
         self.assertEqual(5, result)
+
+    def test_get_largest_region_5by4(self):
+        grid = [
+            [0, 0, 1, 1],
+            [0, 0, 1, 0],
+            [0, 1, 1, 0],
+            [0, 1, 0, 0],
+            [1, 1, 0, 0]
+        ]
+
+        result = get_largest_region(grid)
+        self.assertEqual(8, result)
+
+    def test_get_largest_region_no_rows(self):
+        grid = []
+
+        with self.assertRaises(ValueError):
+            get_largest_region(grid)
+
+    def test_get_largest_region_no_cols(self):
+        grid = [[], []]
+
+        with self.assertRaises(ValueError):
+            get_largest_region(grid)
