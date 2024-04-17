e48aea6a6f8ee76a0a2475f91e46fcabfca02400
Amar <162648880+amar528@users.noreply.github.com>
2024-04-17 18:13:34 +1000

    Merge pull request #31
    * leetcode 75
    
    * leetcode 75
    
    * leetcode 75

--- leetcode-75/Heap/kth_largest_arr_element_215.py
+++ leetcode-75/Heap/kth_largest_arr_element_215.py
@@ -3,5 +3,11 @@ import heapq
 
 class Solution:
     def findKthLargest(self, nums, k):
-        heapq.heapify(nums)
-        return heapq.nlargest(k, nums)[-1]
+        heap = [-num for num in nums]
+        heapq.heapify(heap)
+
+        val = None
+        for i in range(k):
+            val = heapq.heappop(heap)
+
+        return -val
