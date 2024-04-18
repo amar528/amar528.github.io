1b2546497610c00c081ef71ad3f58a6eb82abc78
Amar <162648880+amar528@users.noreply.github.com>
2024-04-18 10:54:34 +1000

    Merge pull request #33
    * leetcode 75
    
    * leetcode 75

--- leetcode-75/Sliding_Window/max_num_vowels_given_substr_len_1456.py
+++ leetcode-75/Sliding_Window/max_num_vowels_given_substr_len_1456.py
@@ -1,5 +1,4 @@
 from collections import Counter
-from functools import reduce
 
 
 class Solution:
@@ -9,17 +8,25 @@ class Solution:
         n = len(s)
         left = 0
         right = left + k
-        max_count = 0
 
-        while right <= n:
-            sub_string = s[left:right]
-            counter = Counter(sub_string)
+        sub_string = s[left:right]
+        counter = Counter(sub_string)
+        max_so_far = sum(map(lambda item: item[1] if item[0] in vowels else 0, counter.items()))
 
-            vowel_count = sum(map(lambda item: item[1] if item[0] in vowels else 0, counter.items()))
+        # left += 1
+        # right += 1
+        max_count = max_so_far
 
-            max_count = max(max_count, vowel_count)
+        while right < n:
+            if s[left] in vowels:
+                max_count -= 1
+
+            if s[right] in vowels:
+                max_count += 1
+
+            max_so_far = max(max_so_far, max_count)
 
             left += 1
             right += 1
 
-        return max_count
+        return max_so_far
