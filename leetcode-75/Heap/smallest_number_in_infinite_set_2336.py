352e33ce3753b443405965a10face98a3e764c8c
amar528 <amar.om@icloud.com>
2024-04-20 09:39:36 +1000

    leetcode 75

---
+++ leetcode-75/Heap/smallest_number_in_infinite_set_2336.py
@@ -0,0 +1,19 @@
+import heapq
+
+
+class SmallestInfiniteSet:
+
+    def __init__(self):
+        self.heap = []
+        heapq.heapify(self.heap)
+        heapq.heappush(self.heap, 1)
+
+    def popSmallest(self) -> int:
+        val = heapq.heappop(self.heap)
+        if not self.heap:
+            heapq.heappush(self.heap, val + 1)
+        return val
+
+    def addBack(self, num: int) -> None:
+        if self.heap and self.heap[0] > num:
+            heapq.heappush(self.heap, num)
