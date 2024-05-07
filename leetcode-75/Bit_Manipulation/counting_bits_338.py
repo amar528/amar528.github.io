40d220178f312acea6744ffee100af1a87d82206
amar528 <amar.om@icloud.com>
2024-05-07 11:26:35 +1000

    Leetcode 75

--- leetcode-75/Bit_Manipulation/counting_bits_338.py
+++ leetcode-75/Bit_Manipulation/counting_bits_338.py
@@ -3,12 +3,16 @@ from typing import List
 
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        if n <= 0:
-            return [0]
+        # initialise result as all 0's
+        result = [0] * (n + 1)
 
-        result = []
+        # iterate from 0 to n, inclusive
         for num in range(n + 1):
-            b = bin(num)
-            result.append(b.count('1'))
+            _shift_idx = num >> 1
+            _shift = result[_shift_idx]
+
+            _and = (num & 1)
+
+            result[num] = _shift + _and
 
         return result
