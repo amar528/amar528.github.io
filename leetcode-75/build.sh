dd9f5ceb8295990a3cf7c3f80d15a17325715e5b
amar528 <amar.om@icloud.com>
2024-05-17 12:50:36 +1000

    Leetcode 75

---
+++ leetcode-75/build.sh
@@ -0,0 +1,15 @@
+cd Array_String || exit
+../.venv/bin/python3 -m unittest
+cd ..
+
+cd Backtracking || exit
+../.venv/bin/python3 -m unittest
+cd ..
+
+cd Binary_Search || exit
+../.venv/bin/python3 -m unittest
+cd ..
+
+cd Binary_Search_Tree || exit
+../.venv/bin/python3 -m unittest
+cd ..
