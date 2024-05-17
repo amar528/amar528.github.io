a18a304c4fbeecf6fbe25c651b50cd8c4bb2abf7
amar528 <amar.om@icloud.com>
2024-05-17 13:01:38 +1000

    Leetcode 75

--- leetcode-75/build.sh
+++ leetcode-75/build.sh
@@ -1,15 +1,30 @@
+#!/usr/bin/env bash
+
+BASE=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Brittle - TODO refactor into /src /test modules and fix imports
+PYTHON=$BASE/.venv/bin/python3
+
 cd Array_String || exit
-../.venv/bin/python3 -m unittest
-cd ..
+$PYTHON -m unittest
+cd $BASE || exit
 
 cd Backtracking || exit
-../.venv/bin/python3 -m unittest
-cd ..
+$PYTHON -m unittest
+cd $BASE || exit
 
 cd Binary_Search || exit
-../.venv/bin/python3 -m unittest
-cd ..
+$PYTHON -m unittest
+cd $BASE || exit
 
 cd Binary_Search_Tree || exit
-../.venv/bin/python3 -m unittest
-cd ..
+$PYTHON -m unittest
+cd $BASE || exit
+
+cd Binary_Tree/DFS || exit
+$PYTHON -m unittest
+cd $BASE || exit
+
+cd Binary_Tree/BFS || exit
+$PYTHON -m unittest
+cd $BASE || exit
