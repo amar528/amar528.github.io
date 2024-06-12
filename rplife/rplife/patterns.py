8da53046edd6d6aa7ee4b514e5c7c667eb883e8c
amar528 <amar.om@icloud.com>
2024-06-13 08:54:03 +1000

    Refactor to individual repositories

--- rplife/rplife/patterns.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Pattern:
-    name: str
-
-    # each tuple represents a coordinate of an alive cell in the grid
-    alive_cells: set[tuple[int, int]]
